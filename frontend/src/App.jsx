2a25ffa871a15979b3477cc269462c062daf28cd
SpaceShell <sluis27901@gmail.com>
2024-11-07 13:42:16 -0800

    Styled and structured quotes section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Message } from "./message";
 import { FormLayout } from "./form";
 import Image from 'react-bootstrap/Image';
 import Logo from "../public/quotebook_logo.png"
+import Form from 'react-bootstrap/Form';
 
 function App() {
 	const [date, setDate] = useState("lastweek")
@@ -15,8 +16,7 @@ function App() {
 
 	return (
 		<div className="App">
-			{/* TODO: include an icon for the quote book */}
-			<div className="lg:w-1/3 min-h-screen md:min-h-fit lg:min-h-screen bg-zinc-800 text-white px-12 md:px-16 py-20 lg:py-10 lg:float-left lg:mr-10 relative">
+			<div className="lg:w-1/3 min-h-screen md:min-h-fit lg:min-h-screen bg-zinc-800 text-white px-10 md:px-16 py-20 lg:py-10 lg:float-left relative">
 				<div className="flex">
 				<Image className="size-12 md:size-14 lg:size-12 bg-white mr-3" src={Logo} roundedCircle/>
 				<h1 className="text-4xl md:text-5xl lg:text-[3vw] lg:leading-[2.5rem] xl:text-4xl align-middle">Quotebook</h1>
@@ -25,20 +25,21 @@ function App() {
 
 				<h2 className="mb-4">Submit a quote</h2>
 				<FormLayout setSubmitted={setSubmitted}/>
-				<p className="mt-40 relative top-12">Hack at UCI Tech Deliverable</p>
+				<p className="mt-40 relative top-12 lg:top-0">Hack at UCI Tech Deliverable</p>
 			</div>
 			<div className="md:h-screen md:overflow-y-auto">
-				<form>
-					<label htmlFor="age">Date posted</label>
-					<select id="age" defaultValue={"lastweek"} onChange={(e) => changeDate(e)} name="age">
-						<option value={"lastweek"}>Last week</option>
-						<option value={"lastmonth"}>Last month</option>
-						<option value={"lastyear"}>Last year</option>
-						<option value={"all"}>All quotes</option>
-					</select>
-				</form>
-
-				<h2>Previous Quotes</h2>
+				<div className="w-full border-b-2 border-solid">
+					<Form className="pr-10 float-right pt-[5%] md:py-2">
+						<Form.Label htmlFor="age">Date posted</Form.Label>
+						<Form.Select aria-label="Date posted" id="age" defaultValue={"lastweek"} onChange={(e) => changeDate(e)} name="age" className="!rounded-2xl">
+							<option value={"lastweek"}>Last week</option>
+							<option value={"lastmonth"}>Last month</option>
+							<option value={"lastyear"}>Last year</option>
+							<option value={"all"}>All quotes</option>
+						</Form.Select>
+					</Form>
+					<h2 className="pl-10 py-4">Previous Quotes</h2>
+				</div>
 				<Message postedDate={date} submitted={submitted}/>
 			</div>
 		</div>
--- frontend/src/message.jsx
+++ frontend/src/message.jsx
@@ -10,19 +10,34 @@ export function Message({postedDate, submitted}) {
 		.then(data => setData(data))
 	}, [postedDate, submitted])
 
-    return(
-        <div className="messages">
-				{data ? (
-					data.quotes.map((data, i) => (
-						<div key={i}>
-							<p>{data.name}</p>
-							<p>{data.message}</p>
-							<p>{data.time}</p>
-						</div>
-					))
-				) : (
-					<p></p>
-				)}
-			</div>
+    // return(
+    //     <div className="md:flex md:flex-wrap md:justify-center px-10 md:px-28 gap-x-16 mb-10">
+	// 		{data ? (
+	// 			data.quotes.map((data, i) => (
+	// 				<div key={i} className="mt-10 shadow-[0_1px_3px_3px_rgb(0,0,0,0.1),_0_5px_2px_-2px_rgb(0,0,0,0.1)] rounded-xl px-10 py-4 basis-1/3 grow">
+	// 					<p>"{data.message}"</p>
+	// 					<p className="text-right">- {data.name}</p>
+	// 					<p className="text-right">{data.time}</p>
+	// 				</div>
+	// 			))
+	// 		) : (
+	// 			<p></p>
+	// 		)}
+	// 	</div>
+    // )
+	return(
+        <div className="md:columns-2 px-10 md:px-28 gap-16 mb-10">
+			{data ? (
+				data.quotes.map((data, i) => (
+					<div key={i} className="shadow-[0_1px_3px_3px_rgb(0,0,0,0.1),_0_5px_2px_-2px_rgb(0,0,0,0.1)] rounded-xl px-10 pt-4 pb-3 h-fit w-full mt-10 inline-block">
+						<p>"{data.message}"</p>
+						<p className="text-right">- {data.name}</p>
+						<p className="text-right">{data.time}</p>
+					</div>
+				))
+			) : (
+				<p></p>
+			)}
+		</div>
     )
 }
