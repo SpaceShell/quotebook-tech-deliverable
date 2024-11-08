085b52a931c15eebafaca156ab2ad15a0b334c1f
SpaceShell <sluis27901@gmail.com>
2024-11-07 20:40:35 -0800

    Added new font styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,11 +19,11 @@ function App() {
 			<div className="lg:w-1/3 min-h-screen md:min-h-fit lg:min-h-screen bg-zinc-800 text-white px-10 md:px-16 py-20 lg:py-10 lg:float-left relative">
 				<div className="flex">
 				<Image className="size-12 md:size-14 lg:size-12 bg-white mr-3" src={Logo} roundedCircle/>
-				<h1 className="text-4xl md:text-5xl lg:text-[3vw] lg:leading-[2.5rem] xl:text-4xl align-middle">Quotebook</h1>
+				<h1 className="text-4xl md:text-5xl lg:text-[3vw] lg:leading-[2.5rem] xl:text-4xl align-middle font-bold">Quotebook</h1>
 				</div>
 				<p className="my-5 ">Welcome to the Hack at UCI Quotebook! Post your quotes and view what others have submitted!</p>
 
-				<h2 className="mb-4">Submit a quote</h2>
+				<h2 className="mb-4 font-bold">Submit a quote</h2>
 				<FormLayout setSubmitted={setSubmitted}/>
 				<p className="mt-40 lg:mt-0 relative top-12 lg:top-24 lg:top-0">Hack at UCI Tech Deliverable</p>
 			</div>
@@ -38,7 +38,7 @@ function App() {
 							<option value={"all"}>All quotes</option>
 						</Form.Select>
 					</Form>
-					<h2 className="pl-10 py-8">Previous Quotes</h2>
+					<h2 className="pl-10 py-8 font-bold">Previous Quotes</h2>
 				</div>
 				<Message postedDate={date} submitted={submitted}/>
 			</div>
